3462ff7c0f5141cf3c9e641e5e176909b127c71c
yusasa16 <yusasa16@gmail.com>
2022-04-24 19:29:51 +0900

    ブログ記事取得

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,40 @@
 <template lang="pug">
   div
     p 作成中
-    div {{ posts }}
+    article(v-for="post in posts" :key="post.id")
+      div.posts
+        h1(v-html='post.title.rendered')
+        div(v-html='post.content.rendered')
 </template>
 
 <script>
-// import { mapMutations } from 'vuex'
-
 export default {
   name: 'IndexPage',
   layout: 'default',
   data() {
     return {
-      // posts: this.$store.state.posts,
-    }
+      posts: []
+    };
   },
-  computed: {
-    posts() {
-      return this.$store.state.posts
-    }
-  }
-  // mounted() {
-  //   axios
-  //     .get('http://www.wp-dummy.yusaku-tech.com/wp-json/wp/v2/posts')
-  //     .then((res) => {
-  //       this.posts = res.data
-  //     })
-  // },
+  mounted() {
+    this.$axios
+      .get('http://www.wp-dummy.yusaku-tech.com/wp-json/wp/v2/posts')
+      .then((res) => {
+        this.posts = res.data
+      })
+  },
 }
 </script>
+
+<style lang="scss">
+.posts {
+  > h1 {
+    font-size: 2.4rem;
+    background: beige;
+  }
+
+  > div {
+    padding: 1em;
+  }
+}
+</style>
--- store/index.js
+++
@@ -1,9 +0,0 @@
-import Vuex from 'vuex'
-// import Vue from 'vue'
-import posts from './posts/index'
-
-export default new Vuex.Store({
-  modules: {
-    posts
-  }
-})
